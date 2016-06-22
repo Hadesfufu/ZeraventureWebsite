9056d99f1d7fa6b859ae35e14d1f9b3b8269af80
HadesFufu <yohan.masson@neuf.fr>
2016-06-22 18:10:58 +0200

    second push

--- public/index.js
+++ public/index.js
@@ -1,6 +1,20 @@
 /**
  * Created by Yohan on 21/06/2016.
  */
+
+function initProgress(elem){
+    var elem = new ProgressBar.Circle(elem, {
+        color: '#23b93c',
+        strokeWidth: 50,
+        svgStyle: {
+            display: 'block',
+            width: '100%'
+        },
+        easing: 'easeOut',
+    });
+    return elem;
+}
+
 $(document).ready(function(){
     $( window ).scroll(function() {
         /*var next = $(".active").next();
@@ -38,5 +52,36 @@ $(document).ready(function(){
                 return false;
             }
         }
+    });
+
+
+
+    var moteur, scenario, graphiques, musique;
+    var moteurValue = $('.moteur > .progressCircle').attr("percent");
+    var scenarioValue = $('.scenario > .progressCircle').attr("percent");
+    var graphiquesValue = $('.graphique > .progressCircle').attr("percent");
+    var musiqueValue = $('.musique > .progressCircle').attr("percent");
+
+    moteur = initProgress(".moteur > .progressCircle");
+    scenario = initProgress(".scenario > .progressCircle");
+    graphiques = initProgress(".graphique > .progressCircle");
+    musique = initProgress(".musique > .progressCircle");
+
+    moteur.animate(moteurValue);
+    moteur.setText(moteurValue * 100 + "%");
+
+    scenario.animate(scenarioValue);
+    scenario.setText(scenarioValue * 100 + "%");
+
+    graphiques.animate(graphiquesValue);
+    graphiques.setText(graphiquesValue * 100 + "%");
+
+    musique.animate(musiqueValue);
+    musique.setText(musiqueValue * 100 + "%");
+
+    $(".progressbar-text").css("color", "white");
+    $(".progressbar-text").css("font-size", "2.3em");
+
+    $('.progressBarContainer').show('slow', function(){
     });
 });
